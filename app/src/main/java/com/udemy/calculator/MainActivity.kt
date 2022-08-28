73ac27f155b2a51de335a51bae436a6df19ea71b
김준기 <gugi329@daum.net>
2022-08-28 16:07:44 +0900

    계산기 UI 완성

--- app/src/main/java/com/udemy/calculator/MainActivity.kt
+++ app/src/main/java/com/udemy/calculator/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.udemy.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.udemy.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
+
+    fun onDigit(view: View) {
+        Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+    }
+
 }
